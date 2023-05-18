b53dda3fe3fe9c65c16eb347e332f8115f282dab
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-05-18 22:49:24 +0600

    page changing problems

--- DotNetWebApp/Program.cs
+++ DotNetWebApp/Program.cs
@@ -7,12 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<DotNetWebAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DotNetWebAppContext") ?? throw new InvalidOperationException("Connection string 'DotNetWebAppContext' not found.")));
@@ -38,7 +40,9 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
